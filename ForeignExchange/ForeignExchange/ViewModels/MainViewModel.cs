bfe29e957629f3d055574697a9b886090136b066
daniel mandarano <danielmandarano@yahoo.com.ar>
2019-03-01 20:12:24 -0300

    3 commit

--- ForeignExchange/ForeignExchange/ViewModels/MainViewModel.cs
+++ ForeignExchange/ForeignExchange/ViewModels/MainViewModel.cs
@@ -1,13 +1,81 @@
-﻿
-namespace ForeignExchange.ViewModels
+﻿namespace ForeignExchange.ViewModels
 {
-   
- public class MainViewModel
+    using Models;
+    using System.Collections.ObjectModel;
+    using System.Windows.Input;
+
+
+
+    public class MainViewModel
     {
+        #region Properties
+        public string Amount
+        {
+            get;
+            set;
+        }
+
+        public ObservableCollection<Rate> Rates
+            { get; 
+            set;
+            }
+
+        public Rate SourceRate
+        {
+            get;
+            set;
+
+        }
+
+        public Rate TargetRate
+        {
+            get;
+            set;
+        }
+        public bool IsRunning
+        {
+            get;
+            set;
+        }
+        public bool IsEnabled
+        {
+            get;
+
+             set;
+            }
+
+        public string Result
+        {
+            get;
+
+            set;
+        }
+
+
+        #endregion
         public MainViewModel()
         {
 
         }
-        }    
- }
+
+        #region Commands
+
+        public ICommand ConvertCommand
+        {
+            get
+            {
+               return new RelayCommand(Convert);
+                
+            }
+
+        }
+         void Convert()
+        {
+
+        }
+        
+        
+        #endregion
+    }
+}
 
