c91931fac181bdb6c90832693666ebb62ad2b351
daniel mandarano <danielmandarano@yahoo.com.ar>
2019-03-05 10:26:35 -0300

    cambio3

--- ForeignExchange/ForeignExchange/ViewModels/MainViewModel.cs
+++ ForeignExchange/ForeignExchange/ViewModels/MainViewModel.cs
@@ -2,14 +2,33 @@
 {
     using GalaSoft.MvvmLight.Command;
     using Models;
+    using Newtonsoft.Json;
     using System;
+    using System.Collections.Generic;
     using System.Collections.ObjectModel;
+    using System.ComponentModel;
+    using System.Net.Http;
     using System.Windows.Input;
 
 
 
-    public class MainViewModel
+    public class MainViewModel : INotifyPropertyChanged
     {
+        #region Events
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+
+        #region Atributos
+
+        bool _isRunning;
+        bool _isEnabled;
+        
+        ObservableCollection<Rate> _rates;
+        string _result;
+        #endregion
+
+
         #region Properties
         public string Amount
         {
@@ -19,8 +38,20 @@
 
         public ObservableCollection<Rate> Rates
         {
-            get;
-            set;
+            get
+            {
+                return _rates;
+            }
+            set
+            {
+                if (_rates != value)
+                {
+                    _rates = value;
+                    PropertyChanged?.Invoke(
+                        this,
+                        new PropertyChangedEventArgs(nameof(Rates)));
+                }
+            }
         }
 
         public Rate SourceRate
@@ -37,29 +68,107 @@
         }
         public bool IsRunning
         {
-            get;
-            set;
+            get
+            {
+                return _isRunning;
+            }
+            set
+            {
+                if(_isRunning !=value)
+                {
+                    _isRunning = value;
+                    PropertyChanged?.Invoke(
+                        this,
+                        new PropertyChangedEventArgs(nameof (IsRunning)));
+                }
+            }
         }
         public bool IsEnabled
         {
-            get;
-
-            set;
+            get
+            {
+                return _isEnabled;
+            }
+            set
+            {
+                if (_isEnabled != value)
+                {
+                    _isEnabled = value;
+                    PropertyChanged?.Invoke(
+                        this,
+                        new PropertyChangedEventArgs(nameof(IsEnabled)));
+                }
+            }
         }
 
         public string Result
         {
-            get;
-
-            set;
+            get
+            {
+                return _result;
+            }
+            set
+            {
+                if (_result != value)
+                {
+                    _result = value;
+                    PropertyChanged?.Invoke(
+                        this,
+                        new PropertyChangedEventArgs(nameof(Result)));
+                }
+            }
         }
 
 
         #endregion
         public MainViewModel()
         {
+            LoadRates();
+        }
+
+        
+
+        #region Metodos
+        async void LoadRates()
+        {
+            IsRunning = true;
+            Result = "Loading Rates....";
+
+            try
+            {
+                var client = new HttpClient();
+                client.BaseAddress = new 
+                    Uri("https://openexchangerates.org");
+                var url = "/api/latest.json?app_id=f490efbcd52d48ee98fd62cf33c47b9e";
+                var response = await client.GetAsync(url);
+                var result = await response.Content.ReadAsStringAsync();
+                if (!response.IsSuccessStatusCode)
+                {
+                    IsRunning = false;
+                    Result = result;
+                }
+
+                var rates = JsonConvert.DeserializeObject<List<Rate>>(result);
+                Rates = new ObservableCollection<Rate>(rates);
+
+                IsRunning = false;
+                IsEnabled = true;
+                Result = "Ready to convert";
+            }
+            catch (Exception ex)
+            {
+                IsRunning = false;
+                Result = ex.Message;
+            }
+
+
 
         }
+
+        #endregion
+
+
+
 
         #region Commands
         public ICommand ConvertCommand
